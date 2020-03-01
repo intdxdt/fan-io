f0eb40b0e2938affe04a5c5c8a90111ffc9c8887
Titus Tienaah <titus.intdxdt@gmail.com>
2020-03-01 23:40:13 +0000

    wip:crossbeam tut

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
+use crossbeam_utils::sync::WaitGroup;
+use std::thread;
+
+fn sample_cross_beam() {
+    // Create a new wait group.
+    let wg = WaitGroup::new();
+
+    for i in 0..4 {
+        // Create another reference to the wait group.
+        let wg = wg.clone();
+
+        thread::spawn(move || {
+            // Do some work.
+            println!("work from thread : {}", i);
+            // Drop the reference to the wait group.
+            drop(wg);
+        });
+    }
+
+    // Block until all threads have finished their work.
+    wg.wait();
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        sample_cross_beam();
     }
 }
